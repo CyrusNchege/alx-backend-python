1bce9c01985a5c6c4fde0eddedf1b4e7d918f24a
CyrusNchege <cyruschegecc@gmail.com>
2023-10-10 16:22:50 +0300

    Add coroutine that will execute async_comprehension four times in parallel using asyncio.gather

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""a python module to measure the execution time"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measure_runtime: measure the execution time
+    Returns:
+        float: the measured time
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end_time = time.time()
+    return end_time - start_time
